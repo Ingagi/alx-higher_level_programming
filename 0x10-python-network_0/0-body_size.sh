359b3eea5da0b2aed73f19119b95c3b04efffdb9
Ingagi <oshumbusho3@gmail.com>
2024-12-19 08:05:10 -0800

    let-us-see

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Script to display the body size of a URL response in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+length=$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}')
+if [ -z "$length" ]; then
+    echo "No Content-Length header found"
+else
+    echo "$length"
+fi
 
